bdba37bace9f3670a9b26bbd3d9382d67c0b2ae2
notagshen <106027330+notagshen@users.noreply.github.com>
2024-09-16 00:40:15 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -33,74 +33,91 @@ EOF
 	echo "Xray 安装完成."
 }
 config_xray() {
-	config_type=$1
-	mkdir -p /etc/xrayL
-	if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
-		echo "类型错误！仅支持socks和vmess."
-		exit 1
-	fi
+    config_type=$1
+    mkdir -p /etc/xrayL
+    if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
+        echo "类型错误！仅支持socks和vmess."
+        exit 1
+    fi
 
-	read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
-	START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-	if [ "$config_type" == "socks" ]; then
-		read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-		SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
+    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
+    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+    if [ "$config_type" == "socks" ]; then
+        read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
+        SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
 
-		read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-		SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
-	elif [ "$config_type" == "vmess" ]; then
-		read -p "UUID (默认随机): " UUID
-		UUID=${UUID:-$DEFAULT_UUID}
-		read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-		WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
-	fi
+        read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
+        SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+    elif [ "$config_type" == "vmess" ]; then
+        read -p "UUID (默认随机): " UUID
+        UUID=${UUID:-$DEFAULT_UUID}
+        read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
+        WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
+    fi
 
-	for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
-		config_content+="[[inbounds]]\n"
-		config_content+="port = $((START_PORT + i))\n"
-		config_content+="protocol = \"$config_type\"\n"
-		config_content+="tag = \"tag_$((i + 1))\"\n"
-		config_content+="[inbounds.settings]\n"
-		if [ "$config_type" == "socks" ]; then
-			config_content+="auth = \"password\"\n"
-			config_content+="udp = true\n"
-			config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
-			config_content+="[[inbounds.settings.accounts]]\n"
-			config_content+="user = \"$SOCKS_USERNAME\"\n"
-			config_content+="pass = \"$SOCKS_PASSWORD\"\n"
-		elif [ "$config_type" == "vmess" ]; then
-			config_content+="[[inbounds.settings.clients]]\n"
-			config_content+="id = \"$UUID\"\n"
-			config_content+="[inbounds.streamSettings]\n"
-			config_content+="network = \"ws\"\n"
-			config_content+="[inbounds.streamSettings.wsSettings]\n"
-			config_content+="path = \"$WS_PATH\"\n\n"
-		fi
-		config_content+="[[outbounds]]\n"
-		config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
-		config_content+="protocol = \"freedom\"\n"
-		config_content+="tag = \"tag_$((i + 1))\"\n\n"
-		config_content+="[[routing.rules]]\n"
-		config_content+="type = \"field\"\n"
-		config_content+="inboundTag = \"tag_$((i + 1))\"\n"
-		config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
-	done
-	echo -e "$config_content" >/etc/xrayL/config.toml
-	systemctl restart xrayL.service
-	systemctl --no-pager status xrayL.service
-	echo ""
-	echo "生成 $config_type 配置完成"
-	echo "起始端口:$START_PORT"
-	echo "结束端口:$(($START_PORT + $i - 1))"
-	if [ "$config_type" == "socks" ]; then
-		echo "socks账号:$SOCKS_USERNAME"
-		echo "socks密码:$SOCKS_PASSWORD"
-	elif [ "$config_type" == "vmess" ]; then
-		echo "UUID:$UUID"
-		echo "ws路径:$WS_PATH"
-	fi
-	echo ""
+    for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
+        config_content+="[[inbounds]]\n"
+        config_content+="port = $((START_PORT + i))\n"
+        config_content+="protocol = \"$config_type\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n"
+        config_content+="[inbounds.settings]\n"
+        if [ "$config_type" == "socks" ]; then
+            config_content+="auth = \"password\"\n"
+            config_content+="udp = true\n"
+            config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+            config_content+="[[inbounds.settings.accounts]]\n"
+            config_content+="user = \"$SOCKS_USERNAME\"\n"
+            config_content+="pass = \"$SOCKS_PASSWORD\"\n"
+        elif [ "$config_type" == "vmess" ]; then
+            config_content+="[[inbounds.settings.clients]]\n"
+            config_content+="id = \"$UUID\"\n"
+            config_content+="[inbounds.streamSettings]\n"
+            config_content+="network = \"ws\"\n"
+            config_content+="[inbounds.streamSettings.wsSettings]\n"
+            config_content+="path = \"$WS_PATH\"\n\n"
+        fi
+        config_content+="[[outbounds]]\n"
+        config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
+        config_content+="protocol = \"freedom\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n\n"
+        config_content+="[[routing.rules]]\n"
+        config_content+="type = \"field\"\n"
+        config_content+="inboundTag = \"tag_$((i + 1))\"\n"
+        config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
+    done
+    echo -e "$config_content" >/etc/xrayL/config.toml
+    systemctl restart xrayL.service
+    systemctl --no-pager status xrayL.service
+
+    # 放行端口
+    for ((port = START_PORT; port <= START_PORT + i - 1; port++)); do
+        iptables -I INPUT -p tcp --dport $port -j ACCEPT
+        iptables -I INPUT -p udp --dport $port -j ACCEPT
+    done
+
+    # 保存 iptables 规则
+    if [ -x "$(command -v iptables-save)" ]; then
+        iptables-save > /etc/iptables.rules
+    elif [ -x "$(command -v service)" ]; then
+        service iptables save
+    elif [ -x "$(command -v netfilter-persistent)" ]; then
+        netfilter-persistent save
+    fi
+
+    echo ""
+    echo "生成 $config_type 配置完成"
+    echo "起始端口:$START_PORT"
+    echo "结束端口:$(($START_PORT + $i - 1))"
+    if [ "$config_type" == "socks" ]; then
+        echo "socks账号:$SOCKS_USERNAME"
+        echo "socks密码:$SOCKS_PASSWORD"
+    elif [ "$config_type" == "vmess" ]; then
+        echo "UUID:$UUID"
+        echo "ws路径:$WS_PATH"
+    fi
+    echo ""
 }
+
 main() {
 	[ -x "$(command -v xrayL)" ] || install_xray
 	if [ $# -eq 1 ]; then
